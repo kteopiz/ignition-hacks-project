e70837c85c1b2018052ed1d71fa9e765ba838026
Kyle Teopiz <kylecteopiz@gmail.com>
2023-08-27 00:43:58 -0400

    fixed profile sections prompts and JS

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const createProfileButton = document.getElementById("createProfile");
 const profileDisplay = document.getElementById("profileDisplay");
 
 createProfileButton.addEventListener("click", () => {
-  const name = document.getElementById("name").value;
+  const fname = document.getElementById("fname").value;
+  const lname = document.getElementById("lname").value;
   const age = document.getElementById("age").value;
   const gender = document.getElementById("gender").value;
   const description = document.getElementById("description").value;
@@ -18,20 +19,22 @@ createProfileButton.addEventListener("click", () => {
   const location = document.getElementById("location").value;
 
   const profileHTML = `
-    <h2>${name}</h2>
+    <h2>${fname} ${lname}</h2> 
     <p>${age}, ${gender}</p>
-    <p>${description}</p>
-    <p>read? ${read}</p>
-    <p>video games? ${videoGames}</p>
-    <p>artsy? ${artistic}</p>
-    <p>party-goer? ${parties}</p>
-    <p>attending campus events? ${campus}</p>
-    <p>favourite tv show genre: ${showGenre}</p>
-    <p>favourite music genre: ${musicGenre}</p>
-    <p>social battery: ${socialBattery}</p>
-    <p>time preferences: ${timePreference}</p>
-    <p>favourite location to go to: ${location}</p>
+    <p>Tell Me About Yourself: ${description}</p>
+    <p>Reading Habits: ${read}</p>
+    <p>Gaming Habits:  ${videoGames}</p>
+    <p>Artsy: ${artistic}</p>
+    <p>Party-Goer: ${parties}</p>
+    <p>Involvement in Campus Events: ${campus}</p>
+    <p>Favourite TV Show Genre: ${showGenre}</p>
+    <p>Favourite Music Genre: ${musicGenre}</p>
+    <p>Personality: ${socialBattery}</p>
+    <p>Early Bird or Night Owl: ${timePreference}</p>
+    <p>Favourite Campus Locations: ${location}</p>
+
   `;
+
   profileDisplay.innerHTML = profileHTML;
   profileDisplay.style.display = "block";
 });
